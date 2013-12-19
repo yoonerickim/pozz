3b8e4b1ab2e43543fce97aefd017dbdd4eb543be
YoonEricKim <jyk.ctw@gmail.com>
2013-12-18 22:35:37 -0800

    Initial Commit pozz

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pozz1::Application.config.secret_token = '5f2fd2648a792512530c019a2edf8695a4b366303478074182528b5c4790c6e871031c5c9c4678b32b2be7bcf59481844751b8e600a465338fa6b401e34320be'
